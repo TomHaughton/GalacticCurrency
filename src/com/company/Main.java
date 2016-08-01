6bd58c83fa8851cde6ff7f7e130f061687bf3d5f
Tom <thomasjhaughton@hotmail.com>
2016-08-01 21:43:04 +0100

    validation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    private static String testValue = "MMCDXCVI";
+    private static String testValue = "IX";
 
     private enum conversionKey {
         I (1, "glob"),
@@ -32,17 +34,51 @@ public class Main {
     public static void main(String[] args) {
 
         char[] numerals = parseNumerals(testValue);
-        List<Integer> arabic = convertToArabic(numerals);
-        Integer cost = calculateCost(arabic);
-        System.out.println(cost.toString());
+        if (validateNumerals(numerals)) {
+            List<Integer> arabic = convertToArabic(numerals);
+            Integer cost = calculateCost(arabic);
+            System.out.println(cost == null ? "Invalid Number" : cost.toString());
+        } else{
+            System.out.println("Invalid number");
+        }
     }
 
-    public static char[] parseNumerals(String rawinput) {
+    private static char[] parseNumerals(String rawinput) {
 
         String rawInput = rawinput.toUpperCase();
         char[] numerals = rawInput.toCharArray();
-        return numerals;
 
+        return numerals;
+    }
+
+    private static boolean validateNumerals(char[] nums){
+
+        char compare = 'A';
+        int count = 0;
+        for (char ch:nums) {
+
+            if (ch != 'I' && ch != 'V' && ch != 'X' &&  ch != 'L' && ch != 'C' && ch != 'D' && ch != 'M'){
+                return false;
+            }
+
+            if (ch == 'I' || ch == 'X' || ch == 'C' || ch == 'M'){
+                if (ch == compare){
+                    count++;
+                    if (count == 3){
+                        return false;
+                    }
+                } else{
+                    compare = 'A';
+                    count = 0;
+                }
+                compare = ch;
+            }
+            else{
+                compare = 'A';
+                count = 0;
+            }
+        }
+        return true;
     }
 
     private static List<Integer> convertToArabic(char[] numerals) {
@@ -62,6 +98,7 @@ public class Main {
         Integer cost = 0;
 
         boolean added = false;
+        boolean subtracted = false;
 
         for (int i = arabic.size() - 1; i > 0; i--) {
             if (arabic.get(i) > arabic.get(i - 1)) {
@@ -69,16 +106,39 @@ public class Main {
                     cost += arabic.get(i);
                 }
                 added = true;
-                cost -= arabic.get(i - 1);
+                if (!subtracted) {
+                    cost -= arabic.get(i - 1);
+                    subtracted = true;
+                } else {
+                    return null;
+                }
             } else if (arabic.get(i) < arabic.get(i - 1)) {
+//                if (subtracted){
+//                    return null;
+//                }
                 if(!added) {
                     cost += arabic.get(i);
                 }
                 added = true;
+                subtracted = false;
                 cost += arabic.get(i - 1);
+            } else if (arabic.get(i).equals(arabic.get(i - 1))) {
+                if (subtracted){
+                    return null;
+                }
+                if(!added) {
+                    cost += arabic.get(i);
+                }
+                cost += arabic.get(i - 1);
+                subtracted = false;
+                added = true;
             } else {
+                if (subtracted){
+                    return null;
+                }
                 cost += arabic.get(i - 1);
                 added = false;
+                subtracted = false;
             }
         }
 
